35db1d3ca1b78759fa00f43d0ed3b21c7a606e88
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-03-06 23:10:38 +0530

    Update alive.py

--- Flare_Robot/modules/alive.py
+++ Flare_Robot/modules/alive.py
@@ -14,9 +14,9 @@ PHOTO = "https://telegra.ph/file/6266d4d7ce030b8a7cf2d.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEX = "**♡ I,m ғʟᴀʀᴇ ʀᴏʙᴏᴛ 愛** \n\n"
+    TEXT = "**♡ I,m ғʟᴀʀᴇ ʀᴏʙᴏᴛ 愛** \n\n"
     TEXT += f"**♡ I'm Working With sᴇxʏ Speed** \n\n"
-    TEXT += f"**♡ ғʟᴀʀᴇ: LATEST Version** \n\n"
+    TEXT += f"**♡ ғʟᴀʀᴇ: LATEST Version = pyrover , tlhver & kontol** \n\n"
     TEXT += f"**♡ My Creator: [ ᴀsᴛᴀ](http://t.me/Asta_silva02)** \n\n"
     TEXT += f"**♡ ᴀɴʏ ɪssᴜᴇs ᴄᴏɴᴛᴀᴄᴛ ʜᴇʀᴇ @Freia_Support** \n\n"
     TEXT += "**♡ ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ 💘💘💘**"
