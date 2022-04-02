bdef442f8cf7927a615f23349919fdda2cc6d51e
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-04-02 20:19:02 +0530

    Update errors.py

--- Flare_Robot/utils/errors.py
+++ Flare_Robot/utils/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from Hottie_Robot import SUPPORT_CHAT, pbot
+from Flare_Robot import SUPPORT_CHAT, pbot
 
 
 def split_limits(text):
