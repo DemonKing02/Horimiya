2c73d27993c6ab04278f3dc49ea2b948e0202202
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-04-02 18:41:56 +0530

    Update notes_sql.py

--- Flare_Robot/modules/sql/notes_sql.py
+++ Flare_Robot/modules/sql/notes_sql.py
@@ -1,10 +1,10 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
+from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 
-from Hottie_Robot.modules.helper_funcs.msg_types import Types
-from Hottie_Robot.modules.sql import BASE, SESSION
+from Flare_Robot.modules.helper_funcs.msg_types import Types
+from Flare_Robot.modules.sql import BASE, SESSION
 
 
 class Notes(BASE):
@@ -15,7 +15,7 @@ class Notes(BASE):
     file = Column(UnicodeText)
     is_reply = Column(Boolean, default=False)
     has_buttons = Column(Boolean, default=False)
-    msgtype = Column(Integer, default=Types.BUTTON_TEXT.value)
+    msgtype = Column(BigInteger, default=Types.BUTTON_TEXT.value)
 
     def __init__(self, chat_id, name, value, msgtype, file=None):
         self.chat_id = str(chat_id)  # ensure string
@@ -30,7 +30,7 @@ class Notes(BASE):
 
 class Buttons(BASE):
     __tablename__ = "note_urls"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
     chat_id = Column(String(14), primary_key=True)
     note_name = Column(UnicodeText, primary_key=True)
     name = Column(UnicodeText, nullable=False)
