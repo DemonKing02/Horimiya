5ffa26c3ed3cbcb51c22ffe17eaadc932d3071cb
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-04-02 15:26:01 +0530

    Update cleaner_sql.py

--- Flare_Robot/modules/sql/cleaner_sql.py
+++ Flare_Robot/modules/sql/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Boolean, Column, UnicodeText
 
-from Hottie_Robot.modules.sql import BASE, SESSION
+from Flare_Robot.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
