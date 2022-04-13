40fff4b07d1c6df6c00a89622d406e50aa6826c6
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-04-13 23:12:36 +0530

    Update google.py

--- Flare_Robot/modules/google.py
+++ Flare_Robot/modules/google.py
@@ -157,7 +157,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Yone <==="
+        app_details += "\n\n❁———FLARE———❁"
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
