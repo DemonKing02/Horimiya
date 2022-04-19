1bb68601f1e26d55f14992ad2841f732a70a9e95
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-04-19 13:12:57 +0530

    Update couples.py

--- Flare_Robot/modules/couples.py
+++ Flare_Robot/modules/couples.py
@@ -68,7 +68,7 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
             couple_selection_message = f"""Couple of the day:
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = 💖
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
-            await pgram.send_message(
+            await pbot.send_message(
                 message.chat.id,
                 text=couple_selection_message
             )
