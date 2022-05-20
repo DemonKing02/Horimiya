2cd32c7bda377fd47b25b9774c84c44744b206a4
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-05-20 20:27:38 +0530

    Update __init__.py

--- Flare_Robot/modules/__init__.py
+++ Flare_Robot/modules/__init__.py
@@ -1,5 +1,11 @@
 import sys
 
+# enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+)
 
 No_LOAD = "cleaner rss connection"
 
